0cb624ab8c35e980d138af3d05115401fd52b2d7
sammoth <sammoth.th@gmail.com>
2026-02-14 16:01:03 +0000

    smoother resize fixes

--- video/out/vulkan/context_moltenvk.m
+++ video/out/vulkan/context_moltenvk.m
@@ -16,8 +16,9 @@
  */
 
 #include <CoreGraphics/CoreGraphics.h>
+#include <Foundation/Foundation.h>
 #include <QuartzCore/CAMetalLayer.h>
-#include <MoltenVK/mvk_vulkan.h>
+#include <stdatomic.h>
 
 #include "common.h"
 #include "context.h"
@@ -26,24 +27,73 @@
 struct priv {
     struct mpvk_ctx vk;
     CAMetalLayer *layer;
+    void *observer;
+    atomic_bool resize_pending;
+    int last_w;
+    int last_h;
 };
+
+@interface MoltenVKLayerObserver : NSObject
+@property(nonatomic, assign) struct vo *vo;
+@property(nonatomic, assign) atomic_bool *resizePending;
+@property(nonatomic, assign) CAMetalLayer *layer;
+- (void)start;
+- (void)stop;
+@end
+
+@implementation MoltenVKLayerObserver
+
+static void *kMoltenVKObserverContext = &kMoltenVKObserverContext;
+
+- (void)start
+{
+    if (!self.layer)
+        return;
+
+    [self.layer addObserver:self
+                 forKeyPath:@"bounds"
+                    options:NSKeyValueObservingOptionNew
+                    context:kMoltenVKObserverContext];
+    [self.layer addObserver:self
+                 forKeyPath:@"contentsScale"
+                    options:NSKeyValueObservingOptionNew
+                    context:kMoltenVKObserverContext];
+}
+
+- (void)stop
+{
+    if (!self.layer)
+        return;
+
+    @try {
+        [self.layer removeObserver:self forKeyPath:@"bounds" context:kMoltenVKObserverContext];
+        [self.layer removeObserver:self forKeyPath:@"contentsScale" context:kMoltenVKObserverContext];
+    } @catch (__unused NSException *e) {
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey, id> *)change
+                       context:(void *)context
+{
+    if (context != kMoltenVKObserverContext) {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+        return;
+    }
+
+    if (self.resizePending)
+        atomic_store(self.resizePending, true);
+    if (self.vo)
+        vo_wakeup(self.vo);
+}
+
+@end
 
 static bool get_layer_size(CAMetalLayer *layer, int *w, int *h)
 {
-    if (!layer || !layer.superlayer)
+    if (!layer)
         return false;
-
-    CGSize s = layer.drawableSize;
-    int dw = (int)s.width;
-    int dh = (int)s.height;
-
-    // Match mpv's MetalLayer workaround: MoltenVK can transiently force
-    // drawableSize to 1x1 while presenting, which causes visible flicker.
-    if (dw > 1 && dh > 1) {
-        *w = dw;
-        *h = dh;
-        return true;
-    }
 
     CGSize bounds = layer.bounds.size;
     CGFloat scale = layer.contentsScale;
@@ -55,12 +105,50 @@ static bool get_layer_size(CAMetalLayer *layer, int *w, int *h)
         return true;
     }
 
+    CGSize s = layer.drawableSize;
+    int dw = (int)s.width;
+    int dh = (int)s.height;
+    if (dw > 1 && dh > 1) {
+        *w = dw;
+        *h = dh;
+        return true;
+    }
+
+    return false;
+}
+
+static bool get_effective_layer_size(struct priv *p, int *w, int *h)
+{
+    int cw = 0;
+    int ch = 0;
+    if (get_layer_size(p->layer, &cw, &ch)) {
+        p->last_w = cw;
+        p->last_h = ch;
+        *w = cw;
+        *h = ch;
+        return true;
+    }
+
+    if (p->last_w > 1 && p->last_h > 1) {
+        *w = p->last_w;
+        *h = p->last_h;
+        return true;
+    }
+
     return false;
 }
 
 static void moltenvk_uninit(struct ra_ctx *ctx)
 {
     struct priv *p = ctx->priv;
+
+    if (p->observer) {
+        MoltenVKLayerObserver *observer = p->observer;
+        [observer stop];
+        [observer release];
+        p->observer = NULL;
+    }
+
     ra_vk_ctx_uninit(ctx);
     mpvk_uninit(&p->vk);
     if (p->layer) {
@@ -74,6 +162,9 @@ static bool moltenvk_init(struct ra_ctx *ctx)
     struct priv *p = ctx->priv = talloc_zero(ctx, struct priv);
     struct mpvk_ctx *vk = &p->vk;
     int msgl = ctx->opts.probing ? MSGL_V : MSGL_ERR;
+    atomic_store(&p->resize_pending, true);
+    p->last_w = 0;
+    p->last_h = 0;
 
     if (ctx->vo->opts->WinID == -1) {
         MP_MSG(ctx, msgl, "WinID missing\n");
@@ -85,6 +176,14 @@ static bool moltenvk_init(struct ra_ctx *ctx)
 
     p->layer = (__bridge CAMetalLayer *)(intptr_t)ctx->vo->opts->WinID;
     CFRetain((__bridge CFTypeRef)p->layer);
+
+    MoltenVKLayerObserver *observer = [MoltenVKLayerObserver new];
+    observer.vo = ctx->vo;
+    observer.resizePending = &p->resize_pending;
+    observer.layer = p->layer;
+    [observer start];
+    p->observer = observer;
+
     VkMetalSurfaceCreateInfoEXT info = {
          .sType = VK_STRUCTURE_TYPE_METAL_SURFACE_CREATE_INFO_EXT,
          .pLayer = p->layer,
@@ -113,9 +212,21 @@ static bool moltenvk_reconfig(struct ra_ctx *ctx)
     struct priv *p = ctx->priv;
     int w = 0;
     int h = 0;
-    if (get_layer_size(p->layer, &w, &h))
+    atomic_store(&p->resize_pending, true);
+    if (get_effective_layer_size(p, &w, &h))
         ra_vk_ctx_resize(ctx, w, h);
     return true;
+}
+
+static void moltenvk_wait_events(struct ra_ctx *ctx, int64_t until_time_ns)
+{
+    int64_t now = mp_time_ns();
+    int64_t max_sleep_until = now + MP_TIME_MS_TO_NS(16);
+
+    if (until_time_ns <= now || until_time_ns > max_sleep_until)
+        until_time_ns = max_sleep_until;
+
+    vo_wait_default(ctx->vo, until_time_ns);
 }
 
 static int moltenvk_control(struct ra_ctx *ctx, int *events, int request, void *arg)
@@ -124,12 +235,15 @@ static int moltenvk_control(struct ra_ctx *ctx, int *events, int request, void *
         struct priv *p = ctx->priv;
         int w = 0;
         int h = 0;
-        if (!get_layer_size(p->layer, &w, &h))
-            return VO_NOTIMPL;
-        if (w != ctx->vo->dwidth || h != ctx->vo->dheight) {
-            ctx->vo->dwidth = w;
-            ctx->vo->dheight = h;
-            *events |= VO_EVENT_RESIZE;
+        bool resize_pending = atomic_exchange(&p->resize_pending, false);
+        bool have_size = get_effective_layer_size(p, &w, &h);
+
+        if (have_size && (w != ctx->vo->dwidth || h != ctx->vo->dheight)) {
+            if (!ra_vk_ctx_resize(ctx, w, h))
+                return VO_ERROR;
+            *events |= VO_EVENT_RESIZE | VO_EVENT_EXPOSE;
+        } else if (resize_pending) {
+            *events |= VO_EVENT_EXPOSE;
         }
     }
     return VO_NOTIMPL;
@@ -140,6 +254,7 @@ const struct ra_ctx_fns ra_ctx_vulkan_moltenvk = {
     .name           = "moltenvk",
     .reconfig       = moltenvk_reconfig,
     .control        = moltenvk_control,
+    .wait_events    = moltenvk_wait_events,
     .init           = moltenvk_init,
     .uninit         = moltenvk_uninit,
 };
